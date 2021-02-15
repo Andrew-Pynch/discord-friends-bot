f3aa713642244dc0c3373f689e693018f94200db
Andrew Pynch <andrewpynchbusiness@gmail.com>
2021-02-14 19:04:26 -0800

    parser is working

--- parser.py
+++ parser.py
@@ -1,16 +1,28 @@
-import csv
+from re import M
+from typing import FrozenSet
 from bs4 import BeautifulSoup
 
-file = open('messages.txt', 'a')
+frostbite_members = {
+    "Zack": "161272422949978113",
+    "Kameron": "179718268494413826",
+    "Bitmin": "210095732785414144",
+    "Chris": "170938761021095936",
+    "Greg": "127215621330042880",
+    "Andrew": "149687867285700608",
+    "Jed": "149686178939928576",
+    "Max": "121306516337852418",
+    "Zain": "140858227049889793",
+}
 
-with open("frostbite.html", "r", encoding="utf8", errors='ignore') as f:
-    
-    contents = f.read()
+html = open("data.html")
+soup = BeautifulSoup(html, "html.parser")
 
-    soup = BeautifulSoup(contents, 'lxml')
+data = []
 
-    file.write(soup.text.strip())
-
-file.close()
-
-
+for member in frostbite_members:
+    message_div = soup.find_all("div", {"class": "chatlog__message-group"})
+    member_title = soup.find_all("span", {"data-user-id": frostbite_members[member]})
+    if message_div and member_title:
+        member_messages = soup.find_all("span", {"class": "markdown"})
+        for message in member_messages:
+            print(f"{member}: {message.text}")
